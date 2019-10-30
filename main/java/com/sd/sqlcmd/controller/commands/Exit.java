4458d5dba8513a43eb30669f755743e5be67c1c0
sergey <serezadorohin@icloud.com>
2019-10-30 22:44:01 +0700

    30.10.2019

---
+++ main/java/com/sd/sqlcmd/controller/commands/Exit.java
@@ -0,0 +1,20 @@
+package com.sd.sqlcmd.controller.commands;
+
+public class Exit implements Command {
+
+    @Override
+    public String description() {
+        return "\033[1;33m" + "\u001B[35m" + "/exit" + "\033[0;37m" + " - выход из программы / отключение от базы данных";
+    }
+
+    @Override
+    public String operation_name() {
+        return "/exit";
+    }
+
+    @Override
+    public String operation(String[] data) {
+        System.exit(0);
+        return null;
+    }
+}
